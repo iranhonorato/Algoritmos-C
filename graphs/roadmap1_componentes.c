ff273fc2f17f5f544181919a71f46b2d7ac23da7
iran <iranilsonhonorato88@gmail.com>
2026-05-11 14:44:26 -0300

    feat: impmentando ocnta componentes em grafos

--- graphs/roadmap1_componentes.c
+++ graphs/roadmap1_componentes.c
@@ -3,34 +3,33 @@
 #include <stdlib.h>
 
 // gcc -Wall -Og -g -fsanitize=address graph.o test_graph.c dfs.c `pkg-config --cflags --libs check`
-void dfs(grafo *G, int nodo_atual, int *visitados, int count) {
-    if (visitados[nodo_atual] != -1) {
+void dfs(grafo *G, int nodo_atual, int *componentes, int count) {
+    if (componentes[nodo_atual] != -1) {
         return;
     }
 
-    visitados[nodo_atual] = count;
+    componentes[nodo_atual] = count;
     for (int prox_nodo = 0; prox_nodo < G->V; prox_nodo++) {
         if (G->edges[nodo_atual][prox_nodo] != 0 && G->edges[nodo_atual][prox_nodo] != INFINITY) {
-            dfs(G, prox_nodo, visitados, count);
+            dfs(G, prox_nodo, componentes, count);
         }
     }
 
     return;
 }
 
-int conta_components(grafo *G, int *visitados) {
+int conta_components(grafo *G, int *componentes) {
     for (int i = 0; i < G->V; i++) {
-        visitados[i] = -1;
+        componentes[i] = -1;
     }
     int count = 0;
 
     for (int nodo_atual = 0; nodo_atual < G->V; nodo_atual++) {
-        if (visitados[nodo_atual] == -1) {
-            dfs(G, nodo_atual, visitados, count);
+        if (componentes[nodo_atual] == -1) {
+            dfs(G, nodo_atual, componentes, count);
             count++;
         } 
     }
 
     return count;
 }
-
